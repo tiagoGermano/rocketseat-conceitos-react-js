08647b516df40fe24b752f476b7f899a205f86f9
Tiago Germano <tiagogermanos@gmail.com>
2021-01-19 23:29:14 -0300

    implemented remove and add new repository

--- src/App.js
+++ src/App.js
@@ -1,28 +1,97 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "./styles.css";
+import api from "services/api"
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+  const [title, setTitle] = useState('');
+  const [url, setUrl] = useState('');
+  const [techs, setTechs] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then( response => {
+      setRepositories(response.data);
+    })
+  },[]);
+
   async function handleAddRepository() {
-    // TODO
+    const repository = {
+      title,
+      url,
+      techs
+    }
+    api.post('repositories', repository).then( response => {
+      const lst = [...repositories];
+      lst.push(response.data);
+
+      setRepositories(lst);
+      clearForm();
+    })
   }
 
+  function clearForm(){
+    setTitle('');
+    setUrl('');
+    setTechs([]);    
+  }
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete('repositories/'+id).then( response => {
+      const index = repositories.findIndex(repo => repo.id === id)
+      const ls = [...repositories]
+      ls.splice(index, 1)
+      setRepositories(ls)
+    })
+  }
+
+  function addOrRemoveTech(tech){
+    const lst = [...techs];
+    const index = lst.indexOf(tech)
+    if(index === -1 ){
+      lst.push(tech)
+    } else {
+      lst.splice(index, 1)
+    }
+
+    setTechs(lst);
   }
 
   return (
     <div>
+      <label style={{fontWeight: 'bold'}}>REPOSITORIES</label>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+          {
+            repositories.map( (repo, i) =>
+              <li key={i}>{repo.title}
+                <button onClick={() => handleRemoveRepository(repo.id)}>
+                  Remover
+                </button>
+              </li>
+            )
+          }
       </ul>
 
+      <br/>
+      <div style={{display : 'inline'}}>
+        <input type="text" placeholder="title" value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <input placeholder="url" value={url} onChange={(e) => setUrl(e.target.value)}/>
+      </div>
+      <div>
+        <span>
+          <input type="checkbox" value="html" onChange={(e) => addOrRemoveTech(e.target.value)}/>
+          <label >HTML</label>
+        </span>
+        <span>
+          <input type="checkbox" value="css" onChange={(e) => addOrRemoveTech(e.target.value)}/>
+          <label >CSS</label>
+        </span>         
+        <span>
+          <input type="checkbox" value="javascript" onChange={(e) => addOrRemoveTech(e.target.value)}/>
+          <label >javascript</label>
+        </span>
+
+      </div>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
   );
